37e6ef4dae7f6ec10c0f513ab45ca60a0d2215a0
Tchilo <tchiloross@gmail.com>
2022-05-05 15:15:14 +0200

    Add validations to reservation table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,8 @@
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :plane
+
+  validates : date_of_reservation, presence: true
+  validates : end_of_reservation, presence: true
+
 end
